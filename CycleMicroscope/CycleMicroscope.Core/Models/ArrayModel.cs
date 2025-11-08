5b1f3e2da7eb627c26ee0061629d65195725e11b
KockAnigilator <jonikm892@gmail.com>
2025-11-08 11:10:41 +0300

    Начало - структура

---
+++ CycleMicroscope/CycleMicroscope.Core/Models/ArrayModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CycleMicroscope.Core.Models
+{
+    public class ArrayModel : INotifyPropertyChanged
+    {
+        private int[] _array = System.Array.Empty<int>();
+        private int _size = 10;
+        private int _minValue = 0;
+        private int _maxValue = 100;
+        private int _threshold = 50;
+
+        public int[] Array
+        {
+            get => _array;
+            set => SetField(ref _array, value);
+        }
+
+        public int Size
+        {
+            get => _size;
+            set => SetField(ref _size, value);
+        }
+
+        public int MinValue
+        {
+            get => _minValue;
+            set => SetField(ref _minValue, value);
+        }
+
+        public int MaxValue
+        {
+            get => _maxValue;
+            set => SetField(ref _maxValue, value);
+        }
+
+        public int Threshold
+        {
+            get => _threshold;
+            set => SetField(ref _threshold, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ CycleMicroscope/CycleMicroscope.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CycleMicroscope.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CycleMicroscope.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c463a222-e1bf-4efe-8d83-7ea073163a7d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
